ccd9c71a7443ca9a6fbed357fe4e285ea27a5364
AdaDRJ <adanjesus679@gmail.com>
2025-02-17 00:21:24 -0400

    Tarea completada

---
+++ ConsoleApp1/ConsoleApp1/Calculadora.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1
+{
+    internal class Calculadora
+    {
+        internal double sumar()
+        {
+            double numero1 = 0;
+            double numero2 = 0;
+            double resultado = 0;
+            try
+            {
+                Console.WriteLine("Ingresa el numero 1");
+                numero1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresa el numero 2");
+                numero2 = Convert.ToDouble(Console.ReadLine());
+                resultado = numero1 + numero2;
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Has ingresado una letra, tendras que comenzar de nuevo " + e.Message);
+            }
+            return resultado;
+
+        }
+        internal double restar()
+        {
+
+            double numero1 = 0;
+            double numero2 = 0;
+            double resultado = 0;
+            try
+            {
+                Console.WriteLine("Ingresa el numero 1");
+                numero1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresa el numero 2");
+                numero2 = Convert.ToDouble(Console.ReadLine());
+                resultado = numero1 - numero2;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Has ingresado una letra, tendras que comenzar de nuevo " + e.Message);
+            }
+            return resultado;
+
+        }
+       internal double multiplicar()
+        {
+            double numero1 = 0;
+            double numero2 = 0;
+            double resultado = 0;
+            try
+            {
+                Console.WriteLine("Ingresa el numero 1");
+                numero1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresa el numero 2");
+                numero2 = Convert.ToDouble(Console.ReadLine());
+                resultado = numero1 * numero2;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Has ingresado una letra, tendras que comenzar de nuevo " + e.Message);
+            }
+            return resultado;
+
+
+        }
+        internal double dividir()
+        {
+
+            double numero1 = 0;
+            double numero2 = 0;
+            double resultado = 0;
+            try
+            {
+                Console.WriteLine("Ingresa el numero 1");
+                numero1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresa el numero 2");
+                numero2 = Convert.ToDouble(Console.ReadLine());
+                if (numero2 == 0)
+                {
+                    Console.WriteLine("No se puede dividir en 0");
+                    return 0;
+                }
+                else { 
+                resultado = numero1 / numero2;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Has ingresado una letra, tendras que comenzar de nuevo " + e.Message);
+            }
+            return resultado;
+
+        }
+        internal double elevarAPotencia()
+        {
+            double baseNumero = 0;
+            double exponente = 0;
+            double resultado = 0;
+            try
+            {
+                Console.WriteLine("Ingresa la base:");
+                baseNumero = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Ingresa el exponente:");
+                exponente = Convert.ToDouble(Console.ReadLine());
+
+                resultado = Math.Pow(baseNumero, exponente);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Has ingresado una letra, tendrás que comenzar de nuevo. " + e.Message);
+            }
+            return resultado;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Globalization;
+using System.Runtime.InteropServices;
+using ConsoleApp1;
+
+Console.WriteLine("Welcome to calculate");
+var calculadora = new Calculadora();
+bool bucle = true;
+while (bucle)
+{
+    Console.WriteLine(@"
+      Elige la opcion que quieres usar:
+
+      1- Sumar
+
+      2- Restar
+
+      3- Multiplicar
+
+      4- Dividir
+
+      5- Elevar un numero a la potencia
+
+      6- Salir");
+    int elecion = 0;
+    try 
+    { 
+        
+         elecion = Convert.ToInt32(Console.ReadLine()); 
+    
+    }
+    catch(Exception e) {
+        Console.WriteLine("Has ingresado una opcion incorrecta, elige de nuevo " + e.Message);
+       
+    }
+    switch (elecion)
+    {
+        case 1:
+            double resultadoSuma = calculadora.sumar();
+            Console.WriteLine($"El resultado de la suma es: {resultadoSuma}");
+            break;
+
+            case 2:
+            double resultadoResta = calculadora.restar();
+            Console.WriteLine($"El resultado de la resta es: {resultadoResta}");
+            break;
+
+        case 3:
+            double resultadoMultiplicacion = calculadora.multiplicar();
+            Console.WriteLine($"El resultado de la multiplicacion es: {resultadoMultiplicacion}");
+            break;
+
+            case 4:
+            double resultadoDividir = calculadora.dividir();
+            
+            Console.WriteLine($"El resultado de la dividir es: {resultadoDividir}");
+            break;
+            case 5:
+            double resultadoPotencia = calculadora.elevarAPotencia();
+            Console.WriteLine($"El resultado de la suma es: {resultadoPotencia}");
+            break;
+
+            case 6:
+                bucle = false;
+            break;
+        default:
+            break;
+    }
+
+
+
+
+}
