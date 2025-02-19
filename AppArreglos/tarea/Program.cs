e40c550eade974c93a09816a93f05e303eed7263
AdaDRJ <adanjesus679@gmail.com>
2025-02-18 21:47:05 -0400

    tarea completada de los arreglos

---
+++ AppArreglos/tarea/Program.cs
@@ -0,0 +1,17 @@
+﻿ Console.WriteLine("Vamos a digitar 10 numeros y luego ordenarlos de menor a mayor");
+ double[] arreglos = new double[10];
+
+for (int i = 0; i < arreglos.Length; i++)
+{
+    Console.Write($"Digite el número {i + 1}: ");
+    arreglos[i] = double.Parse(Console.ReadLine());
+}
+ Array.Sort(arreglos);
+
+Console.WriteLine("\nNúmeros ordenados de menor a mayor:");
+foreach (double num in arreglos)
+{
+   Console.WriteLine(num);
+}
+
+
