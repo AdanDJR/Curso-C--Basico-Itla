dd45852ffc443f3bac6351c7af57d5db800d12ad
AdaDRJ <adanjesus679@gmail.com>
2025-02-22 23:39:28 -0400

    tarea completada de la fecha

---
+++ ProgramaDeFecha/ProgramaDeFecha/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa tu fecha de nacimiento (YYYY-MM-DD): ");
+        string fecha = Console.ReadLine();
+
+        if (DateTime.TryParse(fecha, out DateTime fechaNacimiento))
+        {
+            Console.WriteLine($"Naciste un {fechaNacimiento.ToString("dddd")}");
+        }
+        else
+        {
+            Console.WriteLine("Pon el formato correcto");
+        }
+    }
+}
+
+
+
+
+
